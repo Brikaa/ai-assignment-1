e0a4b92ab58b8711372016ca4bdedca873c94e2e
Omar Brikaa <brikaaomar@gmail.com>
2023-03-20 09:44:08 +0200

    Remove cuts, fixed task 5

--- main.pl
+++ main.pl
@@ -6,7 +6,7 @@ is_friend(X, Y) :-
 is_friend(X, Y) :-
     friend(Y, X).
 
-my_member(X, [X | _]) :- !.
+my_member(X, [X | _]).
 my_member(X, [_ | Ys]) :-
     my_member(X, Ys).
 
@@ -21,16 +21,15 @@ friend_list(X, Acc, Acc) :-
 friend_list(X, Acc, Xs) :-
     is_friend(X, Y),
     \+ my_member(Y, Acc),
-    !,
     friend_list(X, [Y | Acc], Xs).
 
-friend_list(X, Xs) :- friend_list(X, [], Xs), !.
+friend_list(X, Xs) :- friend_list(X, [], Xs).
 
 count([], Acc, Acc).
 count([_ | Xs], Acc, N) :-
     Accp is Acc + 1,
     count(Xs, Accp, N).
-count(Xs, N) :- !, count(Xs, 0, N).
+count(Xs, N) :- count(Xs, 0, N).
 
 friend_list_count(X, N) :-
     friend_list(X, Ys),
@@ -43,20 +42,19 @@ people_you_may_know(X, Y) :-
     \+(X=Y),
     \+is_friend(X, Y).
 
-count_commons(_, [], Acc, Acc) :- !.
+count_commons(_, [], Acc, Acc).
 count_commons(Xs, [Y | Ys], Acc, N) :-
     my_member(Y, Xs),
-    !,
     Accp is Acc + 1,
     count_commons(Xs, Ys, Accp, N).
 count_commons(Xs, [Y | Ys], Acc, N) :-
     \+my_member(Y, Xs),
-    !,
     count_commons(Xs, Ys, Acc, N).
-count_commons(Xs, Ys, N) :- !, count_commons(Xs, Ys, 0, N).
+count_commons(Xs, Ys, N) :- count_commons(Xs, Ys, 0, N).
 
 people_you_may_know(X, N, Y) :-
     friend_list(X, Xs),
     friend_list(Y, Ys),
     count_commons(Xs, Ys, Np),
-    N =< Np.
+    N =< Np,
+    \+(X = Y).
